08b98ee2e1cb506a40e022da6694670061f47d30
RedKit ❄️ <98268472+Skips1@users.noreply.github.com>
2025-04-11 14:51:58 +0300

    Update gaiabot.py

--- gaiabot.py
+++ gaiabot.py
@@ -73,3 +73,4 @@ QUESTIONS = [
 "What options do I have for adding custom fonts and typography to my website using a no-code platform?" ,
 "Can I use a no-code platform to build a website for a personal blog or diary?" ,
 "How can I optimize my website for search engines using a no-code platform?" ,
+"How to use no-code for building applications." ,
