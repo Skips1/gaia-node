522ca643daa975dab1b3dbbde4f3997792382fce
RedKit ❄️ <98268472+Skips1@users.noreply.github.com>
2025-05-11 15:46:10 +0300

    Update gaiabot.py

--- gaiabot.py
+++ gaiabot.py
@@ -111,3 +111,4 @@ QUESTIONS = [
 "How do I handle integration with other applications and systems in my application using a no-code platform?" ,
 "How do I handle integration with social media platforms in my application using a no-code platform?" ,
 "How do I handle performance optimization in my application using a no-code platform?" ,
+"How do I handle user engagement" ,
