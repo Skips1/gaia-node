bea6810b28d5bcd01009688049160c7e7df9850d
RedKit ❄️ <98268472+Skips1@users.noreply.github.com>
2025-05-13 14:33:14 +0300

    Update gaiabot.py

--- gaiabot.py
+++ gaiabot.py
@@ -114,3 +114,4 @@ QUESTIONS = [
 "How do I handle user engagement" ,
 "How do I handle user segmentation and targeting in my application using a no-code platform?" ,
 "How do I handle user personalization in my application using a no-code platform?" ,
+"How do I handle user onboarding in my application using a no-code platform?" ,
