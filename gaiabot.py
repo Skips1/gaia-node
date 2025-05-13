d1d194205eba8e4f4b652ff621c41fd38431d469
RedKit ❄️ <98268472+Skips1@users.noreply.github.com>
2025-05-13 14:33:05 +0300

    Update gaiabot.py

--- gaiabot.py
+++ gaiabot.py
@@ -113,3 +113,4 @@ QUESTIONS = [
 "How do I handle performance optimization in my application using a no-code platform?" ,
 "How do I handle user engagement" ,
 "How do I handle user segmentation and targeting in my application using a no-code platform?" ,
+"How do I handle user personalization in my application using a no-code platform?" ,
